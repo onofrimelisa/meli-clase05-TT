d30cd5c587a8486c04d7a9ae67f57e473c696e5c
Melisa Ailén Onofri <onofrimelisa@gmail.com>
2021-02-25 09:34:42 -0300

    feat: change QuickSort algorithm from Recursive to Iterative

--- src/QuickSortSorterImple.java
+++ src/QuickSortSorterImple.java
@@ -26,16 +26,46 @@ public class QuickSortSorterImple<T> implements Sorter<T> {
     }
 
    public void quickSort(T arr[], int begin, int end, Comparator<T> c) {
-        if (begin < end) {
-            int partitionIndex = partition(arr, begin, end, c);
+       int[] stack = new int[end - begin + 1];
 
-            quickSort(arr, begin, partitionIndex-1, c);
-            quickSort(arr, partitionIndex+1, end, c);
-        }
+       // initialize top of stack
+       int top = -1;
+
+       // push initial values of l and h to stack
+       stack[++top] = begin;
+       stack[++top] = end;
+
+       // Keep popping from stack while is not empty
+       while (top >= 0) {
+           // Pop h and l
+           end = stack[top--];
+           begin = stack[top--];
+
+           // Set pivot element at its correct position
+           // in sorted array
+           int p = partition(arr, begin, end, c);
+
+           // If there are elements on left side of pivot,
+           // then push left side to stack
+           if (p - 1 > begin) {
+               stack[++top] = begin;
+               stack[++top] = p - 1;
+           }
+
+           // If there are elements on right side of pivot,
+           // then push right side to stack
+           if (p + 1 < end) {
+               stack[++top] = p + 1;
+               stack[++top] = end;
+           }
+       }
     }
 
     @Override
     public void sort(T[] arr, Comparator<T> c) {
+        System.out.println("Sorting using Quick Sort algorithm.");
+        System.out.println();
+
         this.quickSort(arr, 0, arr.length - 1, c);
     }
 
@@ -43,8 +73,16 @@ public class QuickSortSorterImple<T> implements Sorter<T> {
         System.out.println("Testing Quick Sort algorithm...");
         QuickSortSorterImple sorter = new QuickSortSorterImple();
         Integer arr[] = {5, 18, 100, 589, 2, 0, 0, 75};
+
+        System.out.println("The array before sorting is the folowing: ");
+        System.out.println(Arrays.toString(arr));
+        System.out.println();
+
         Comparator<Integer> comparator = (a, b) -> a - b;
         sorter.sort(arr, comparator);
+
+        System.out.println("The array after sorting is the folowing: ");
         System.out.println(Arrays.toString(arr));
+        System.out.println();
     }
 }
