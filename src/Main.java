9da7e504bbb391aa93554a275772d2b96775711b
Melisa Ailén Onofri <onofrimelisa@gmail.com>
2021-02-25 10:17:33 -0300

    feat: finish first approach of the Sorters implementation with Timer

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,18 +9,28 @@ public class Main {
         try {
             Sorter sorter = sorterFactory.getInstance();
 
-            System.out.println("Testing sorting algorithm with integers...");
-            Integer arr[] = {5, 18, 100, 589, 2, 0, 0, 75};
-            System.out.println("The array before sorting is the folowing: ");
-            System.out.println(Arrays.toString(arr));
-            System.out.println();
+            System.out.println("Testing sorting algorithm with an array of 100000 integers...");
+
+            Integer[] array= new Integer[100000];
+            Random random = new Random();
+
+            // Initialize array
+            for (int i =0; i< array.length; i++)
+            {
+                array[i] = random.nextInt(100000)+1;
+            }
+
+            Timer timer = new Timer();
 
             Comparator<Integer> comparator = (a, b) -> a - b;
-            sorter.sort(arr, comparator);
+            timer.start();
+            sorter.sort(array, comparator);
+            timer.end();
 
             System.out.println("The array after sorting is the folowing: ");
-            System.out.println(Arrays.toString(arr));
+            System.out.println(Arrays.toString(array));
             System.out.println();
+            System.out.println("Elapsed Time: " + timer.elapsedTime());
 
         }catch (Exception e) {
             e.printStackTrace();
